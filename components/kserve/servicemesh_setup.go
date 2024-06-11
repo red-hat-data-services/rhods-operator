c42f5a4d9b1001b4edae6648dded803d0fb1cd90
Bartosz Majsak <bartosz.majsak@gmail.com>
2024-06-11 19:31:44 +0200

    chore: renames manifests source to location (#1050)
    - Source is already used elsewhere in Feature DSL, so we might want to
      avoid confusion
    - Location fits the purpose of this field better
    
    (cherry picked from commit 892183903b0154ba663daafa3dec976d2b2563d1)

--- components/kserve/servicemesh_setup.go
+++ components/kserve/servicemesh_setup.go
@@ -40,7 +40,7 @@ func (k *Kserve) defineServiceMeshFeatures(cli client.Client) feature.FeaturesPr
 		if authorinoInstalled {
 			kserveExtAuthzErr := feature.CreateFeature("kserve-external-authz").
 				For(handler).
-				ManifestSource(Resources.Source).
+				ManifestsLocation(Resources.Location).
 				Manifests(
 					path.Join(Resources.ServiceMeshDir, "activator-envoyfilter.tmpl.yaml"),
 					path.Join(Resources.ServiceMeshDir, "envoy-oauth-temp-fix.tmpl.yaml"),
@@ -55,6 +55,19 @@ func (k *Kserve) defineServiceMeshFeatures(cli client.Client) feature.FeaturesPr
 			}
 		} else {
 			fmt.Println("WARN: Authorino operator is not installed on the cluster, skipping authorization capability")
+		}
+
+		temporaryFixesErr := feature.CreateFeature("kserve-temporary-fixes").
+			For(handler).
+			ManifestsLocation(Resources.Location).
+			Manifests(
+				path.Join(Resources.ServiceMeshDir, "grpc-envoyfilter-temp-fix.tmpl.yaml"),
+			).
+			WithData(servicemesh.ClusterDetails).
+			Load()
+
+		if temporaryFixesErr != nil {
+			return temporaryFixesErr
 		}
 
 		return nil
