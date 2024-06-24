5c8e23c84c0959d616c962851a62389d63e6e333
Bartosz Majsak <bartosz.majsak@gmail.com>
2024-06-24 09:13:56 +0200

    chore(lint): updates to latest version (#1074)
    Fixes
    
    - removed irrelavant //nolint comments (see config updates below)
    - converted unused func params to blank identifier when impl does not
      need it
    
    New linters
    
    perfsprint
    
    - disabled fmt.Sprintf checks for one element
    - adjusted errors creation to use errors.New instead of fmt.Errorf when
      no-arg strings are used as messages
    
    Configuration updates
    
    - configured revive to not complain about dot-imports for testing
      libraries
    - adjusted configuration yaml due to changes in the structure
      (skip-dirs)
    - some deprecated linters were removed from golangci-lint toolchain so they are now gone from the config too
    
    Signed-off-by: Max Whittingham <mwhittingham@redhat.com>

--- tests/integration/features/tracker_int_test.go
+++ tests/integration/features/tracker_int_test.go
@@ -68,8 +68,8 @@ var _ = Describe("Feature tracking capability", func() {
 				verificationFeatureErr := feature.CreateFeature("precondition-fail").
 					For(handler).
 					UsingConfig(envTest.Config).
-					PreConditions(func(f *feature.Feature) error {
-						return fmt.Errorf("during test always fail")
+					PreConditions(func(_ *feature.Feature) error {
+						return errors.New("during test always fail")
 					}).
 					Load()
 
@@ -100,8 +100,8 @@ var _ = Describe("Feature tracking capability", func() {
 				verificationFeatureErr := feature.CreateFeature("post-condition-failure").
 					For(handler).
 					UsingConfig(envTest.Config).
-					PostConditions(func(f *feature.Feature) error {
-						return fmt.Errorf("during test always fail")
+					PostConditions(func(_ *feature.Feature) error {
+						return errors.New("during test always fail")
 					}).
 					Load()
 
