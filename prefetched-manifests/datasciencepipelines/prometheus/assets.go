41d46af3d857bff9151d5512dedf7bb54c46419a
Openshift-AI DevOps <openshift-ai-devops@redhat.com>
2026-09-25 07:03:56 +0000

    Updating the operator repo with latest images and manifests

--- prefetched-manifests/datasciencepipelines/prometheus/assets.go
+++ prefetched-manifests/datasciencepipelines/prometheus/assets.go
@@ -19,12 +19,11 @@ limitations under the License.
 package prometheus
 
 import (
+	_ "embed"
 	"fmt"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/yaml"
-
-	_ "embed"
 )
 
 //go:embed datasciencepipelines-prometheusrules.yaml
